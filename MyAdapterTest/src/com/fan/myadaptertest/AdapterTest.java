7bbde5fe291fbbc7cddfb734f05a8ebc22b0cde6
zhangff <zhangff@m15.cn>
2014-09-13 18:17:45 +0800

    Update

---
+++ MyAdapterTest/src/com/fan/myadaptertest/AdapterTest.java
@@ -0,0 +1,15 @@
+package com.fan.myadaptertest;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class AdapterTest extends ArrayAdapter<String> {
+
+	public AdapterTest(Context context, int resource, int textViewResourceId,
+			String[] as) {
+		super(context, resource, textViewResourceId, as);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
